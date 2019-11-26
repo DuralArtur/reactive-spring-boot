3bae4de401d0a1bd9a08e90999206fc04c767f6e
Artur Durał <artur.dural@twoupdigital.com>
2019-11-26 20:48:41 +0100

    initial commit

---
+++ src/test/kotlin/com/r2rek/stockservice/StockServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.r2rek.stockservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
